fa478a62dfc2e19f88fb0f9b9a5be2a1cc1df47c
Olivia Isaacs <oisaacs@farmersfridge.com>
2019-04-17 08:56:19 -0500

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+# TODO: Create required symlinks
+
+# Disable chime on AC adapter connect
+defaults write com.apple.PowerChime ChimeOnNoHardware -bool true
+killall PowerChime
+
+# TODO: Additional setup scripts
